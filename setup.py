8dc041998ada14a5ce4d135d38fbfa03e60edf78
Da-Emp66 <asherbean10@gmail.com>
2025-10-18 20:22:19 -0400

    make setup require <=3.13 python

--- setup.py
+++ setup.py
@@ -16,6 +16,6 @@ setup(
     long_description_content_type="text/markdown",
     author="Anonymous",
     packages=find_packages(),
-    python_requires=">=3.6",
+    python_requires=">=3.6,<=3.13",
     install_requires=requirements,
 )
